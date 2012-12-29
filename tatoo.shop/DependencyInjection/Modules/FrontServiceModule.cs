fdc912a7985cbfb040c698d9d198fd4a009c9553
swstwix <swstwix@gmail.com>
2012-12-29 23:26:06 +0300

    second

---
+++ tatoo.shop/DependencyInjection/Modules/FrontServiceModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+using tatoo.shop.FrontServices;
+using tatoo.shop.FrontServices.Concret;
+
+namespace tatoo.shop.DependencyInjection.Modules
+{
+    public class FrontServiceModule : NinjectModule 
+    {
+        public override void Load()
+        {
+            Bind<ITatooShopFrontService>().To<TatooShopFrontService>();
+        }
+    }
+}
---
+++ tatoo.shop/DependencyInjection/NinjectDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Parameters;
+
+namespace tatoo.shop.DependencyInjection
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType, new IParameter[0]);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType, new IParameter[0]);
+        }
+    }
+}
